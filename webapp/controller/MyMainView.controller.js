ac5df9ca9472923a8f81c47f4f00f98f4ff6a6f6
dayan3847 <dayan3847@gmail.com>
2021-07-08 10:45:18 -0400

    adding buttons

--- webapp/controller/MyMainView.controller.js
+++ webapp/controller/MyMainView.controller.js
@@ -1,18 +1,25 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-	function (Controller) {
-		"use strict";
+        "sap/ui/core/mvc/Controller"
+    ],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.invoices.controller.MyMainView", {
-			onInit: function () {
-                const myJsonModel =  new sap.ui.model.json.JSONModel();
+        return Controller.extend("logaligroup.invoices.controller.MyMainView", {
+            onInit: function () {
+                const myJsonModel = new sap.ui.model.json.JSONModel();
                 const myView = this.getView();
                 myJsonModel.loadData("./model/selectionScreenMenu.json");
                 myView.setModel(myJsonModel, "selectionScreen");
-			}
-		});
-	});
+            },
+            onFilter: function (oEvent) {
+            },
+            onClearFilters: function () {
+                const myModelSs = this.getView().getModel("selectionScreen");
+                myModelSs.setProperty("/CountryKey","");
+                myModelSs.setProperty("/ShipName","");
+            }
+        });
+    });
