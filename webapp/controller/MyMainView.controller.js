55033b7210bc6342e01bd32f00a949cb2eec324a
dayan3847 <dayan3847@gmail.com>
2021-07-14 12:06:01 -0400

    course ok in local

--- webapp/controller/MyMainView.controller.js
+++ webapp/controller/MyMainView.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/Filter",
+        "sap/ui/model/FilterOperator"
     ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("logaligroup.invoices.controller.MyMainView", {
@@ -15,11 +19,28 @@ sap.ui.define([
                 myView.setModel(myJsonModel, "selectionScreen");
             },
             onFilter: function (oEvent) {
+                const oData = this.getView().getModel('selectionScreen').getData();
+                let filters = [];
+                if (oData.ShipName !== "") {
+                    filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName));
+                }
+                if (oData.CountryKey !== "") {
+                    filters.push(new Filter("Country", FilterOperator.EQ, oData.CountryKey));
+                }
+
+                const oBinding = this.getBindingOfList();
+                oBinding.filter(filters);
             },
             onClearFilters: function () {
                 const myModelSs = this.getView().getModel("selectionScreen");
-                myModelSs.setProperty("/CountryKey","");
-                myModelSs.setProperty("/ShipName","");
+                myModelSs.setProperty("/CountryKey", "");
+                myModelSs.setProperty("/ShipName", "");
+                const oBinding = this.getBindingOfList();
+                oBinding.filter([]);
+            },
+            getBindingOfList: function () {
+                const oList = this.getView().byId("invoicesList");
+                return oList.getBinding("items");
             }
         });
     });
